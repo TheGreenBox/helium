79a57b5983aee09d335c95aaae4cd26a283dfb3a
AKindyakov <akindyakov@gmail.com>
2013-05-14 13:28:35 +0400

    add first step test

---
+++ tests/first_step/first_step_main.cpp
@@ -0,0 +1,15 @@
+/* ========================================================
+ * Organization: The Green Box
+ * 
+ * Project name: Helium
+ * File name:  first_step_main.cpp 
+ * Description:
+ * Author:  AKindyakov 
+ * ========================================================
+ */
+
+
+int mian ( )
+{
+    return 0;
+}
