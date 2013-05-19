3a5bb7cd7f1c9976ddfca00228fb4b1f86c42aa4
AKindyakov <akindyakov@gmail.com>
2013-05-19 15:09:54 +0400

    add main cpp

---
+++ game/main.cpp
@@ -0,0 +1,15 @@
+/* ========================================================
+ * Organization: The Green Box
+ * 
+ * Project name: Helium
+ * File name:  game/main.cpp 
+ * Description:
+ * Author:  AKindyakov 
+ * ========================================================
+ */
+
+
+int main (int argsNum, char** argAr)
+{
+    return 0;
+}
