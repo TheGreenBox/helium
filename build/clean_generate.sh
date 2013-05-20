07e732e520073f28a1c0a5de7cfb330fd34536f9
AKindyakov <akindyakov@gmail.com>
2013-05-20 22:47:28 +0400

    add example for Polycode finder and std args to first step test

---
+++ build/clean_generate.sh
@@ -0,0 +1,2 @@
+echo remove product directiry
+rm -f -r product/
--- tests/first_step/src/main.cpp
+++ tests/first_step/src/main.cpp
@@ -11,5 +11,10 @@
 
 int main (int argsNum, char** argAr)
 {
+    std::cout << "The arguments to " << pszArgs[0] <<  "are:\n";
+    for (int i = 1; i < nNumberofArgs; i++)
+    {
+        std::cout << i << " : " << pszArgs[i] << "\n";
+    }
     return 0;
 }
