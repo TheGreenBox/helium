56519ec228dea5e7f4cfd613c2e42a800927ad2d
AKindyakov <akindyakov@gmail.com>
2013-06-01 15:08:47 +0400

    first not work ver of polycode finder cmake module

--- cmake/FindPolycode.cmake
+++ cmake/FindPolycode.cmake
@@ -1,20 +1,47 @@
 
-SET(BULLET_SEARCH_PATHS
-	${USER_HOME}/source/polycode/Polycode/Release/${Linux}/Framework/Core/lib
-	${USER_HOME}/source/polycode/Polycode/Release/${Linux}/Framework/Core/include
-	${USER_HOME}/source/polycode/Polycode/Release/${Linux}/Framework/Modules/lib
-	${USER_HOME}/source/polycode/Polycode/Release/${Linux}/Framework/Modules/include
-)
+SET ( POLYCODE_SEARCH_PATHS
+	  ${CMAKE_HOME_DIRECTORY}/../polycode/Polycode/Release/${CMAKE_SYSTEM_NAME}/Framework/Core/lib
+	  ${CMAKE_HOME_DIRECTORY}/../polycode/Polycode/Release/${CMAKE_SYSTEM_NAME}/Framework/Core/include
+	  ${CMAKE_HOME_DIRECTORY}/../polycode/Polycode/Release/${CMAKE_SYSTEM_NAME}/Framework/Modules/lib
+	  ${CMAKE_HOME_DIRECTORY}/../polycode/Polycode/Release/${CMAKE_SYSTEM_NAME}/Framework/Modules/include )
 
-# - Try to find Bullet
+# - Try to find Polycode
 # Once done this will define
 #
-#  POLYCODE_FOUND - system has bullet
-#  POLYCODE_INCLUDE_DIR - the bullet include directory
+#  POLYCODE_FOUND - system has Polycode
+#  POLYCODE_INCLUDE_DIR - the POLYCODE include directory
 #  POLYCODE_LIBRARIES - Link these to use Collada DOM
 #
 
-FIND_PATH ( BULLET_INCLUDE_DIR NAMES btBulletCollisionCommon.h
+# POLYCODE_CORE_INCLUDE_DIR
+# LIB_POLYCODE_CORE
+
+# list of polycode modules
+
+# POLYCODE_MODULES_INCLUDE_DIR
+#
+# LIB_POLYCODE_2D_PHYSICS
+# LIB_POLYCODE_2D_PHYSICS_D
+# LIB_POLYCODE_3D_PHYSICS
+# LIB_POLYCODE_3D_PHYSICS_D
+# LIB_POLYCODE_TUIO
+# LIB_POLYCODE_TUIO_D
+# LIB_POLYCODE_UI
+# LIB_POLYCODE_UI_D
+
+#libPolycore.a
+#libPolycore_d.a
+
+#libPolycode2DPhysics.a
+#libPolycode2DPhysics_d.a
+#libPolycode3DPhysics.a
+#libPolycode3DPhysics_d.a
+#libPolycodeTUIO.a
+#libPolycodeTUIO_d.a
+#libPolycodeUI.a
+#libPolycodeUI_d.a
+
+FIND_PATH ( POLYCODE_CORE_INCLUDE_DIR NAMES PolyCore.h
 	        HINTS
 	        NO_DEFAULT_PATH
     	    NO_CMAKE_ENVIRONMENT_PATH
@@ -22,35 +49,78 @@ FIND_PATH ( BULLET_INCLUDE_DIR NAMES btBulletCollisionCommon.h
 	        NO_SYSTEM_ENVIRONMENT_PATH
 	        NO_CMAKE_PATH
 	        CMAKE_FIND_FRAMEWORK NEVER
-	        PATHS ${BULLET_SEARCH_PATHS} );
+	        PATHS ${POLYCODE_SEARCH_PATHS} )
 
-FIND_LIBRARY(LIBBULLETDYNAMICS
-  NAMES 
-  ${BULLETDYNAMICS}
-	HINTS
-	NO_DEFAULT_PATH
-	NO_CMAKE_ENVIRONMENT_PATH
-	NO_CMAKE_SYSTEM_PATH
-	NO_SYSTEM_ENVIRONMENT_PATH
-	NO_CMAKE_PATH
-	CMAKE_FIND_FRAMEWORK NEVER
-	$ENV{BULLETDIR}
-	$ENV{BULLET_PATH}
-	PATH_SUFFIXES lib lib64 win32/Dynamic_Release "Win32/${MSVC_YEAR_NAME}/x64/Release" "Win32/${MSVC_YEAR_NAME}/Win32/Release"
-	PATHS ${BULLET_SEARCH_PATHS}
-)
+FIND_LIBRARY  ( LIB_POLYCODE_CORE NAME "libPolycore"
+	            HINTS
+                NO_DEFAULT_PATH
+                NO_CMAKE_ENVIRONMENT_PATH
+                NO_CMAKE_SYSTEM_PATH
+                NO_SYSTEM_ENVIRONMENT_PATH
+                NO_CMAKE_PATH
+                CMAKE_FIND_FRAMEWORK NEVER
+                $ENV{POLYCODEDIR}
+                $ENV{POLYCODE_PATH}
+                PATH_SUFFIXES lib lib64 win32/Dynamic_Release "Win32/${MSVC_YEAR_NAME}/x64/Release" "Win32/${MSVC_YEAR_NAME}/Win32/Release"
+                PATHS ${POLYCODE_SEARCH_PATHS} )
 
-SET(BULLET_LIBRARIES ${LIBBULLETMULTITHREADED} ${LIBBULLETSOFTBODY} ${LIBBULLETDYNAMICS} ${LIBBULLETCOLLISION} ${LIBBULLETMATH})
+FIND_LIBRARY  ( LIB_POLYCODE_CORE_DEBUG NAME "libPolycore_d"
+	            HINTS
+                NO_DEFAULT_PATH
+                NO_CMAKE_ENVIRONMENT_PATH
+                NO_CMAKE_SYSTEM_PATH
+                NO_SYSTEM_ENVIRONMENT_PATH
+                NO_CMAKE_PATH
+                CMAKE_FIND_FRAMEWORK NEVER
+                $ENV{POLYCODEDIR}
+                $ENV{POLYCODE_PATH}
+                PATH_SUFFIXES lib lib64 win32/Dynamic_Release "Win32/${MSVC_YEAR_NAME}/x64/Release" "Win32/${MSVC_YEAR_NAME}/Win32/Release"
+                PATHS ${POLYCODE_SEARCH_PATHS} )
 
-SET(BULLET_LIBRARIES_DEBUG ${LIBBULLETMULTITHREADED_DEBUG} ${LIBBULLETSOFTBODY_DEBUG} ${LIBBULLETDYNAMICS_DEBUG} ${LIBBULLETCOLLISION_DEBUG} ${LIBBULLETMATH_DEBUG})
+# found polycode lib modules
+FIND_PATH ( POLYCODE_MODULES_INCLUDE_DIR NAMES Polycode3DPhysics.h
+	        HINTS
+	        NO_DEFAULT_PATH
+    	    NO_CMAKE_ENVIRONMENT_PATH
+	        NO_CMAKE_SYSTEM_PATH
+	        NO_SYSTEM_ENVIRONMENT_PATH
+	        NO_CMAKE_PATH
+	        CMAKE_FIND_FRAMEWORK NEVER
+	        PATHS ${POLYCODE_SEARCH_PATHS} )
 
-IF(BULLET_INCLUDE_DIR AND BULLET_LIBRARIES)
-  SET(BULLET_FOUND TRUE)
-ENDIF(BULLET_INCLUDE_DIR AND BULLET_LIBRARIES)
+# founding Polycode 3D Physics module
+FIND_LIBRARY  ( LIB_POLYCODE_3D_PHYSICS NAME "libPolycode3DPhysics"
+	            HINTS
+                NO_DEFAULT_PATH
+                NO_CMAKE_ENVIRONMENT_PATH
+                NO_CMAKE_SYSTEM_PATH
+                NO_SYSTEM_ENVIRONMENT_PATH
+                NO_CMAKE_PATH
+                CMAKE_FIND_FRAMEWORK NEVER
+                $ENV{POLYCODEDIR}
+                $ENV{POLYCODE_PATH}
+                PATH_SUFFIXES lib lib64 win32/Dynamic_Release "Win32/${MSVC_YEAR_NAME}/x64/Release" "Win32/${MSVC_YEAR_NAME}/Win32/Release"
+                PATHS ${POLYCODE_SEARCH_PATHS} )
 
-# show the BULLET_INCLUDE_DIR and BULLET_LIBRARIES variables only in the advanced view
-IF(BULLET_FOUND)
-  MESSAGE ("Bullet found (${BULLET_INCLUDE_DIR}) (${BULLET_LIBRARIES}) (${BULLET_LIBRARIES_DEBUG})")
-  MARK_AS_ADVANCED(BULLET_INCLUDE_DIR BULLET_LIBRARIES BULLET_LIBRARIES_DEBUG)
-ENDIF(BULLET_FOUND)
+FIND_LIBRARY  ( LIB_POLYCODE_3D_PHYSICS_DEBUG NAME "libPolycode3DPhysics_d"
+	            HINTS
+                NO_DEFAULT_PATH
+                NO_CMAKE_ENVIRONMENT_PATH
+                NO_CMAKE_SYSTEM_PATH
+                NO_SYSTEM_ENVIRONMENT_PATH
+                NO_CMAKE_PATH
+                CMAKE_FIND_FRAMEWORK NEVER
+                $ENV{POLYCODEDIR}
+                $ENV{POLYCODE_PATH}
+                PATH_SUFFIXES lib lib64 win32/Dynamic_Release "Win32/${MSVC_YEAR_NAME}/x64/Release" "Win32/${MSVC_YEAR_NAME}/Win32/Release"
+                PATHS ${POLYCODE_SEARCH_PATHS} )
+
+IF(POLYCODE_CORE_INCLUDE_DIR AND LIB_POLYCODE_CORE AND LIB_POLYCODE_CORE_DEBUG)
+  SET(POLYCODE_CORE_FOUND TRUE)
+ENDIF(POLYCODE_CORE_INCLUDE_DIR AND LIB_POLYCODE_CORE AND LIB_POLYCODE_CORE_DEBUG)
+
+# show the POLYCODE_INCLUDE_DIR and POLYCODE_LIBRARIES variables only in the advanced view
+IF(POLYCODE_CORE_FOUND)
+  MESSAGE ("POLYCODE found (${POLYCODE_CORE_INCLUDE_DIR}))")
+ENDIF(POLYCODE_CORE_FOUND)
 
