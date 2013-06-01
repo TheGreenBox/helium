22e9b824b6330d8c35331056af6cc0abc33044d7
AKindyakov <akindyakov@gmail.com>
2013-06-02 01:27:06 +0400

    add empty header for first step

---
+++ tests/first_step/include/firstStepTest.h
@@ -0,0 +1,9 @@
+/* ========================================================
+ * Organization: The Green Box
+ * 
+ * Project name: Helium
+ * File name:  firstStepTest.h 
+ * Description:
+ * Author:  AKindyakov 
+ * ========================================================
+ */
