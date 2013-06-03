15fbf174e21a9e1c85fc1bcf1e22f5aefc37ef36
AKindyakov <akindyakov@gmail.com>
2013-06-04 01:27:55 +0400

    some

--- tests/first_step/include/firstStepTest.h
+++
@@ -1,9 +0,0 @@
-/* ========================================================
- * Organization: The Green Box
- * 
- * Project name: Helium
- * File name:  firstStepTest.h 
- * Description:
- * Author:  AKindyakov 
- * ========================================================
- */
