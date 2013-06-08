3dec3fb8053c30c13d83ee756415bdd71f505cc5
SergNikitin <snikitin@outlook.com>
2013-06-08 23:44:04 +0400

    - added win_x64_build.bat

---
+++ build/win_x64_build.bat
@@ -0,0 +1,27 @@
+@title helium for Win: bulding script
+type ..\readme.txt
+
+call win_clean.bat
+
+@echo ------------------------------------------------------
+@echo Generating makefiles
+@echo ------------------------------------------------------
+@echo Creating product directory...
+
+@cd ../
+@md product
+@cd product
+@md debug
+@cd debug
+@echo.
+
+cmake ../../
+@echo.
+
+@echo ------------------------------------------------------
+@echo Building
+@echo ------------------------------------------------------
+
+@make
+
+@pause
