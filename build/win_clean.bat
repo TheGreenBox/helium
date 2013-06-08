04738f81dc31e1932b960bc14f69e61a1c1fe097
SergNikitin <snikitin@outlook.com>
2013-06-08 22:02:52 +0400

    - deleted pauses

--- build/win_clean.bat
+++ build/win_clean.bat
@@ -1,10 +1,7 @@
 @title helium for Win: removing script
-@echo Close this script if you don't want to clean helium directory
-@pause
 @echo Removing /product directory...
 @echo . . .
 @echo . .
 @echo .
 @rmdir /s /q %CD%\product
 @echo All done!
-@pause
