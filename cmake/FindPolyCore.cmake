428eca5036bc673c1f96c6554ddfdde468fb4ae5
AKindyakov <akindyakov@gmail.com>
2013-06-10 00:35:58 +0400

    fix unfounding libz and other libs #1

--- cmake/FindPolyCore.cmake
+++ cmake/FindPolyCore.cmake
@@ -17,30 +17,64 @@ SET ( POLYCODE_SEARCH_PATHS
 # - Try to find Polycode
 # Once done this will define
 
+#// posible names for libpng 
+SET ( LIB_PNG_POSIBLE_NAMES 
+        "png"
+        "png15"
+        "png15_static" 
+    )
+
+SET ( LIB_PNG_POSIBLE_DEBUG_NAMES 
+        "pngd"
+        "png15d"
+        "png15_staticd" 
+    )
+
+#// posible names for zpng 
+SET ( LIB_Z_POSIBLE_NAMES 
+        "z"
+        "zlib"
+    )
+
+SET ( LIB_Z_POSIBLE_DEBUG_NAMES 
+        "z"
+        "zlibd"
+    )
+
+#// posible names for OpenAL 
+SET ( LIB_OPENAL_POSIBLE_NAMES 
+        "openal"
+        "OpenAL32d"
+    )
+
+SET ( LIB_OPENAL_POSIBLE_DEBUG_NAMES 
+        "openald"
+        "OpenAL32"
+    )
+
 SET ( POLLYCORE_LIB_NAMES
-      "Polycore"
-      "freetype"
-      "libvorbisfile"
-      "libvorbis"
-      "libogg"
-      "openal"
-      "physfs"
-      "png15"
-      "z"
+        "Polycore"
+        "freetype"
+        "libvorbisfile"
+        "libvorbis"
+        "libogg"
+        ${LIB_OPENAL_POSIBLE_NAMES}
+        "physfs"
+        ${LIB_PNG_POSIBLE_NAMES}
+        ${LIB_Z_POSIBLE_NAMES}
     )
 
 SET ( POLLYCORE_LIB_DEBUG_NAMES
-      "Polycore_d"
-      "freetype_d"
-      "libvorbisfiled"
-      "libvorbisd"
-      "liboggd"
-      "openald"
-      "physfsd"
-      "png15d"
-      "z"
+        "Polycore_d"
+        "freetype_d"
+        "libvorbisfiled"
+        "libvorbisd"
+        "liboggd"
+        ${LIB_OPENAL_POSIBLE_DEBUG_NAMES}
+        "physfsd"
+        ${LIB_PNG_POSIBLE_DEBUG_NAMES}
+        ${LIB_Z_POSIBLE_DEBUG_NAMES}
     )
-
 
 FIND_PATH ( POLYCODE_CORE_CORE_INCLUDE_DIR NAMES PolyCore.h
             HINTS
@@ -86,7 +120,7 @@ SET ( POLYCODE_CORE_INCLUDE_DIR
 
 FOREACH( LIBRARY ${POLLYCORE_LIB_NAMES} )
     
-#    MESSAGE(STATUS ${LIBRARY})
+    #MESSAGE(STATUS ${LIBRARY})
     FIND_LIBRARY  ( LIB_${LIBRARY} NAME ${LIBRARY}
                     HINTS
                     NO_DEFAULT_PATH
@@ -101,12 +135,15 @@ FOREACH( LIBRARY ${POLLYCORE_LIB_NAMES} )
                     PATHS ${POLYCODE_SEARCH_PATHS} 
                   )
     
-#    MESSAGE(STATUS ${LIB_${LIBRARY}})
+    #MESSAGE(STATUS ${LIB_${LIBRARY}})
     
-    set ( LIB_POLYCODE_CORE  
-          ${LIB_POLYCODE_CORE}
-          ${LIB_${LIBRARY}}
-        )
+    IF( LIB_${LIBRARY} )
+        MESSAGE(STATUS ${LIBRARY} " was found")
+        set ( LIB_POLYCODE_CORE  
+                ${LIB_POLYCODE_CORE}
+                ${LIB_${LIBRARY}}
+            )
+    ENDIF( LIB_${LIBRARY} )
 
 ENDFOREACH( LIBRARY ${POLLYCORE_LIB_NAMES} )
 
@@ -114,7 +151,7 @@ ENDFOREACH( LIBRARY ${POLLYCORE_LIB_NAMES} )
 
 FOREACH( LIBRARY ${POLLYCORE_LIB_DEBUG_NAMES} )
     
-#    MESSAGE(STATUS ${LIBRARY})
+    #MESSAGE(STATUS ${LIBRARY})
     FIND_LIBRARY  ( LIB_${LIBRARY} NAME ${LIBRARY}
                     HINTS
                     NO_DEFAULT_PATH
@@ -129,12 +166,15 @@ FOREACH( LIBRARY ${POLLYCORE_LIB_DEBUG_NAMES} )
                     PATHS ${POLYCODE_SEARCH_PATHS} 
                   )
     
-#    MESSAGE(STATUS ${LIB_${LIBRARY}})
+    #MESSAGE(STATUS ${LIB_${LIBRARY}})
     
-    set ( LIB_POLYCODE_CORE_DEBUG
-          ${LIB_POLYCODE_CORE_DEBUG}
-          ${LIB_${LIBRARY}}
-        )
+    IF( LIB_${LIBRARY} )   
+        MESSAGE(STATUS ${LIBRARY} " DEBUG was found")
+        set ( LIB_POLYCODE_CORE_DEBUG
+                ${LIB_POLYCODE_CORE_DEBUG}
+                ${LIB_${LIBRARY}}
+            )
+    ENDIF( LIB_${LIBRARY} )
 
 ENDFOREACH( LIBRARY ${POLLYCORE_LIB_DEBUG_NAMES} )
 
