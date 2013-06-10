e8ed9be14ca59c5b563b32d72bc10bd917f69e56
Sergey Nikitin <snikitin@outlook.com>
2013-06-10 12:03:40 +0400

    - building script fixed #1

--- build/win_clean.bat
+++ build/win_clean.bat
@@ -6,7 +6,7 @@
 @set folderToDelete=..\product
 
 @if Exist "%folderToDelete%\*.*" (
-    @rmdir /s /q ..\product
+    @rd /s /q ..\product
     @echo All done!
 ) Else (
     @Echo Folder %folderToDelete% does not exist, nothing to clean!
