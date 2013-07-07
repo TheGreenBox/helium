76c95944b6085ca85f951d9c6cd5e38cfe7fcac1
AKindyakov <akindyakov@gmail.com>
2013-07-07 16:15:49 +0400

    put to key hanler core pointer

--- tests/3d_poly_physics/src/wrapClasses.cpp
+++ tests/3d_poly_physics/src/wrapClasses.cpp
@@ -9,3 +9,49 @@
  */
 
 #include "wrapClasses.h"
+
+namespace P = Polycode;
+
+void KeyboardUserInput::handleEvent(Polycode::Event *e) {
+    if(e->getDispatcher() == core->getInput()) {
+        switch(e->getEventCode()) {
+            case P::InputEvent::EVENT_MOUSEDOWN:
+                this->mouseKeyDOWN( (P::InputEvent*)e );
+            break;
+            
+            case P::InputEvent::EVENT_MOUSEUP:
+                this->mouseKeyUP( (P::InputEvent*)e );
+            break;
+			
+            case P::InputEvent::EVENT_KEYDOWN:
+                this->keyDOWN( (P::InputEvent*)e );
+            break;
+			
+            case P::InputEvent::EVENT_KEYUP:
+                this->keyUP( (P::InputEvent*)e );
+            break;
+        }
+    }
+}
+
+void KeyboardUserInput::mouseKeyUP(Polycode::InputEvent*){
+}
+
+void KeyboardUserInput::mouseKeyDOWN(Polycode::InputEvent*){
+}
+
+void KeyboardUserInput::keyUP(Polycode::InputEvent*){
+}
+
+void KeyboardUserInput::keyDOWN(Polycode::InputEvent*){
+}
+
+ProGameobject::ProGameobject( P::PolycodeView* view )
+        : keysHandler(core, &worldobjects, &gameState) {
+    using namespace P; // fucking macros !
+	core = new POLYCODE_CORE(view, 640,480,false,false,0,0,90);
+};
+
+ProGameobject::~ProGameobject(){
+};
+
