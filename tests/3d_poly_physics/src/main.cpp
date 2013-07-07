86ce677a0dd0cac7bb3ee73532b2c5f4639c086a
AKindyakov <akindyakov@gmail.com>
2013-07-07 13:00:41 +0400

    add test for learning polycode 3d physics #8

---
+++ tests/3d_poly_physics/src/main.cpp
@@ -0,0 +1,27 @@
+/* ========================================================
+ * Organization: The Green Box
+ * 
+ * Project name: Helium
+ * File name:  first_step_main.cpp 
+ * Description:
+ * Author:  AKindyakov 
+ * ========================================================
+ */
+
+#include <iostream> 
+
+#include <Polycode.h>
+#include "PolycodeView.h"
+
+#include "mainMacro.h"
+
+#ifdef OS_WIN32
+   #include "windows.h"
+   #include "tchar.h"
+#endif
+
+APP_MAIN_FINCTION
+{
+    std::cout << "template test"; 
+    return 0;
+}
