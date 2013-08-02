177c3170b20b54bb86a6313f54b08f1196c1a3f5
AKindyakov <akindyakov@gmail.com>
2013-08-02 11:29:14 +0400

    add resources path

---
+++ modules/util/pathGlobal.h
@@ -0,0 +1,13 @@
+/* ========================================================
+ * Organization: The Green Box
+ * 
+ * Project name: Helium
+ * File name:  mainLoop.h 
+ * Description:
+ * Author:  AKindyakov 
+ * ========================================================
+ */
+
+#define HELIUM_RESOURCE_PATH "helium_resource"
+#define HELIUM_RESOURCE_PATH_FROM_TEST "../helium_resource"
+
