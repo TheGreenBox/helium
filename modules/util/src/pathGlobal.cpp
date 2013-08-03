4c82cdeab391e1440eb639e3f0430e3e41702b54
AKindyakov <akindyakov@gmail.com>
2013-08-03 18:42:26 +0400

    fix global path -> from macros definitions to const variables

---
+++ modules/util/src/pathGlobal.cpp
@@ -0,0 +1,15 @@
+/* ========================================================
+ * Organization: The Green Box
+ * 
+ * Project name: Helium
+ * File name:  pathGlobal.cpp 
+ * Description:
+ * Author:  AKindyakov 
+ * ========================================================
+ */
+
+#include "pathGlobal.h"
+
+const char* HELIUM_RESOURCE_PATH           = "helium_resource";
+const char* HELIUM_RESOURCE_PATH_FROM_TEST = "../helium_resource";
+
