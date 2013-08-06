3dea76af250d2c111bf92b8d282261cdce336367
AKindyakov <akindyakov@gmail.com>
2013-08-06 12:37:21 +0400

    fix classes for helium pbjects #8 and #14

--- modules/heliumSceneObjects/include/heliumSceneObjects.h
+++ modules/heliumSceneObjects/include/heliumSceneObjects.h
@@ -13,8 +13,28 @@
 
 #include <list>
 
-class WorldObjects;
-class CommonWorldObjects;
+SceneMesh *mesh = new SceneMesh("ninja.mesh");
+    mesh->loadTexture("ninja.png");
+    mesh->loadSkeleton("ninja.skeleton");
+    mesh->getSkeleton()->addAnimation("Run", "run.anim");
+    mesh->getSkeleton()->playAnimation("Run");
+    scene->addEntity(mesh);
+
+
+ScenePrimitive *box = new ScenePrimitive(ScenePrimitive::TYPE_BOX, 4,4,6);  
+    box->setPitch(25.0);
+    box->setPosition(7,-1.05, 0.0);
+    box->setColor(0.5,0.5,1.0,1.0);
+    box->loadTexture("green_texture.png");
+    scene->addPhysicsChild(box, PhysicsSceneEntity::SHAPE_BOX, 0.0);
+
+
+testBox = new ScenePrimitive(ScenePrimitive::TYPE_BOX, 4, 4, 4);
+    testBox->loadTexture("Resources/pink_texture.png");
+    testBox->setColor(0.3,0.5, 1.0,0.4);
+    testBox->setPosition(-5,2,7);
+    scene->addCollisionChild(testBox, PhysicsSceneEntity::SHAPE_BOX);
+
 
 class ObjectBehavior {
 public:
@@ -25,49 +45,69 @@ private:
     
 };
 
-class ImmaterialObject {
-public:    
-    ImmaterialObject(){};
-    virtual ~ImmaterialObject(){};
-
-private:
-};
-
-class ImmaterialIntellectualObject {
+class ObjectDamage {
 public:
-    ImmaterialIntellectualObject(){};
-    virtual ~ImmaterialIntellectualObject(){};
-
-private:
-    ObjectBehavior behavior;
-};
-
-
-class StaticIntellectualObject {
-public:    
-    StaticIntellectualObject(){};
-    virtual ~StaticIntellectualObject(){};
-
+    ObjectDamage(){};
+    virtual ~ObjectDamage(){};
+    void takeThePulse();
 private:
     long int damage;
+    
+};
+
+class StaticObject {
+public:    
+    StaticObject(){};
+    virtual ~StaticObject(){};
+
+private:
+    Polycode::SceneMesh* pModel;
 };
 
 class DynamicObject {
 public:    
-    DynamicObject(){};
+    DynamicObject();
+    DynamicObject(Polycode::SceneMesh*);
+    
     virtual ~DynamicObject(){};
-
-private:
-    long int damage;
+    
+protected:
+    int priorityLevel;
+    ObjectDamage*   pPulse;
+    ObjectBehavior* pMind;
 };
 
-class DynamicIntellectualObject {
+class PhysicObject : public DynamicObject {
 public:    
-    DynamicIntellectualObject(){};
-    virtual ~DynamicIntellectualObject(){};
+    PhysicObject();
+    PhysicObject(Polycode::SceneMesh*);
+    
+    virtual ~PhysicObject();
 
 private:
-    long int damage;
+	Polycode::PhysicsSceneEntity*  pEntity;
+};
+
+class CollisionOnlyObject : public DynamicObject{
+public:    
+    CollisionOnlyObject();
+    CollisionOnlyObject(Polycode::SceneMesh*);
+    
+    virtual ~CollisionOnlyObject();
+
+private:
+    Polycode::CollisionSceneEntity* pEntity;
+};
+
+class ImmaterialObject : public DynamicObject{
+public:    
+    ImmaterialObject();
+    ImmaterialObject(Polycode::SceneMesh*);
+    
+    virtual ~ImmaterialObject();
+
+private:
+    Polycode::SceneMesh* pEntity;
 };
 
 class WorldObjects {
@@ -82,6 +122,8 @@ public:
     virtual ~CommonWorldObjects(){};
 
 private:
+    std::list<StaticObject*>  staticiObjects;
+    std::list<DynamicObject*> dynamicObject;
 };
 
 #endif // _HELIUM_SCENE_OBJECTS_INCLUDED_
--- modules/heliumSceneObjects/src/heliumSceneObjects.cpp
+++ modules/heliumSceneObjects/src/heliumSceneObjects.cpp
@@ -10,4 +10,92 @@
 
 #include "heliumSceneObjects.h"
 
+/*SceneMesh *mesh = new SceneMesh("ninja.mesh");
+    mesh->loadTexture("ninja.png");
+    mesh->loadSkeleton("ninja.skeleton");
+    mesh->getSkeleton()->addAnimation("Run", "run.anim");
+    mesh->getSkeleton()->playAnimation("Run");
+    scene->addEntity(mesh);
+
+
+ScenePrimitive *box = new ScenePrimitive(ScenePrimitive::TYPE_BOX, 4,4,6);  
+    box->setPitch(25.0);
+    box->setPosition(7,-1.05, 0.0);
+    box->setColor(0.5,0.5,1.0,1.0);
+    box->loadTexture("green_texture.png");
+    scene->addPhysicsChild(box, PhysicsSceneEntity::SHAPE_BOX, 0.0);
+
+
+testBox = new ScenePrimitive(ScenePrimitive::TYPE_BOX, 4, 4, 4);
+    testBox->loadTexture("Resources/pink_texture.png");
+    testBox->setColor(0.3,0.5, 1.0,0.4);
+    testBox->setPosition(-5,2,7);
+    scene->addCollisionChild(testBox, PhysicsSceneEntity::SHAPE_BOX);
+*/
+
+ObjectBehavior::ObjectBehavior() {
+}
+
+ObjectBehavior::~ObjectBehavior() {
+}
+
+void ObjectBehavior::thinkAndDo() {
+}
+
+ObjectDamage::ObjectDamage() {
+}
+
+ObjectDamage::~ObjectDamage() {
+}
+
+void ObjectDamage::takeThePulse() {
+}
+
+StaticObject::StaticObject() {
+}
+
+StaticObject::~StaticObject() {
+}
+
+DynamicObject::DynamicObject() {
+}
+
+DynamicObject::DynamicObject(Polycode::SceneMesh*) {
+}
+
+DynamicObject::~DynamicObject() {
+}
+    
+PhysicObject::PhysicObject() {
+}
+
+PhysicObject::PhysicObject(Polycode::SceneMesh*) {
+}
+
+PhysicObject::~PhysicObject() {
+}
+
+CollisionOnlyObject::CollisionOnlyObject() {
+}
+
+CollisionOnlyObject::CollisionOnlyObject(Polycode::SceneMesh*) {
+}
+
+CollisionOnlyObject::~CollisionOnlyObject() {
+}
+
+ImmaterialObject::ImmaterialObject() {
+}
+
+ImmaterialObject::ImmaterialObject(Polycode::SceneMesh*) {
+}
+
+ImmaterialObject::~ImmaterialObject() {
+}
+
+CommonWorldObjects::CommonWorldObjects() { 
+}
+
+CommonWorldObjects::~CommonWorldObjects() {
+}
 
