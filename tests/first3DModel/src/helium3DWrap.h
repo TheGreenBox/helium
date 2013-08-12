6353f949621ea253cadc720d7c7bff32f0682d44
AKindyakov <akindyakov@gmail.com>
2013-08-12 11:10:29 +0400

    fix destructor name after previous refactoring

--- tests/first3DModel/src/helium3DWrap.h
+++ tests/first3DModel/src/helium3DWrap.h
@@ -13,6 +13,7 @@
 
 #include <Polycode.h>
 #include "PolycodeView.h"
+#include "PolyCollisionScene.h"
 #include "Polycode3DPhysics.h"
 
 class Helium3DModel
@@ -21,6 +22,17 @@ public:
 	
     Helium3DModel( Polycode::Mesh* pmh );
     virtual ~Helium3DModel();
+    
+private:
+    
+};
+
+class HeliumPhysical3DModel
+{
+public:
+	
+    HeliumPhysical3DModel( Polycode::Mesh* pmh );
+    virtual ~HeliumPhysical3DModel();
     
 private:
     
