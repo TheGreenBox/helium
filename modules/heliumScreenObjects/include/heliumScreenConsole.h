ed703f4ee259b3f736c74e40f5a04158858633ad
AKindyakov <akindyakov@gmail.com>
2013-08-18 23:52:54 +0400

    carry out Screen Helium Console to module

--- modules/heliumScreenObjects/include/heliumScreenConsole.h
+++ modules/heliumScreenObjects/include/heliumScreenConsole.h
@@ -7,3 +7,70 @@
  * Author:  AKindyakov 
  * ========================================================
  */
+#include <list>
+
+#include <Polycode.h>
+
+class ScreenConsole {
+public:
+
+    /**
+    * Create ScreenConsole
+    * @param _textSize
+    * @param _maxVisibleLines
+    * @param _stackSize
+    * @param _lineSpacing 
+    * @param _defaultColor
+    */ 
+    ScreenConsole( int _textSize,
+             int _maxVisibleLines,
+             int _stackSize,
+             double _lineSpacing = 0.0,
+             Polycode::Vector3 _defaultColor = Polycode::Vector3(0,0,0) );
+    
+    virtual ~ScreenConsole();
+   
+    /**
+    *
+    */ 
+    void setPosition( Polycode::Vector3 _defaultColor = Polycode::Vector3(0,0,0) ){};
+   
+    /**
+    *
+    */ 
+    void add(const char*);
+    void add(const Polycode::String&);
+    
+    /**
+    * Remove elements from the top
+    * @param numToRemove number of removed elements, 
+    *        if numToRemove > 0 - remove from the top of history 
+    *        if numToRemove < 0 - remove from the bottom of history 
+    *        if numToRemove = 0 - remove all history 
+    */
+    void clean(int numToRemove=0);
+    
+    /**
+    * Add new line to the ScreenConsole
+    */
+    void newLine();
+    
+    /**
+    *
+    */ 
+    void scroll(int);
+    
+private:
+    std::list< Polycode::ScreenLabel* > lines;
+    Polycode::Screen* screen;
+    
+    double lineSpacing;
+    int    textSize;
+    int    maxVisibleLines;
+    int    maxLines;
+    Polycode::Vector3 defaultColor;
+
+    int lines_count;
+    int visible_lines;
+};
+
--- modules/heliumScreenObjects/src/heliumScreenConsole.cpp
+++ modules/heliumScreenObjects/src/heliumScreenConsole.cpp
@@ -7,3 +7,96 @@
  * Author:  AKindyakov 
  * ========================================================
  */
+
+#include <iostream>
+#include <list>
+
+#include "heliumScreenConsole.h"
+
+namespace P = Polycode;
+
+ScreenConsole::ScreenConsole(int _textSize, int _maxVisibleLines, int _stackSize, double _lineSpacing, P::Vector3 _defaultColor) 
+    : screen(new P::Screen()),
+        lines_count(0),
+        visible_lines(0),
+        lineSpacing(_lineSpacing),
+        textSize(_textSize),
+        maxVisibleLines(_maxVisibleLines),
+        maxLines(_stackSize),
+        defaultColor(_defaultColor) {
+    this->newLine();
+    this->add("Helium ScreenConsole :");
+    this->newLine();
+}
+
+ScreenConsole::~ScreenConsole() {
+    this->clean();
+}
+
+void ScreenConsole::add(const char* str) {
+    lines.back()->setText(lines.back()->getText() + str);
+}
+
+void ScreenConsole::add(const Polycode::String& str) {
+    lines.back()->setText(lines.back()->getText() + str);
+}
+
+void ScreenConsole::clean(int num) {
+    if ( num == 0 ) {
+        for ( std::list< P::ScreenLabel* >::iterator it = lines.end(); it != lines.begin(); --it ) {
+            screen->removeChild(*it);
+            delete *it;
+        }
+        lines.resize(0);
+    }
+    else {
+        if ( num > 0 ) {
+            for ( ; num != 0; --num ) {
+                screen->removeChild(lines.front());
+                delete lines.front();
+                lines.pop_front();
+            }
+        }
+        else {
+            for ( ; num != 0; ++num ) {
+                screen->removeChild(lines.back());
+                delete lines.back();
+                lines.pop_back();
+            }
+        }
+    }
+}
+
+void ScreenConsole::newLine() {
+    //P::String lineBegin = P::String::IntToString(lines_count);
+    //lineBegin += " : ";
+    //P::ScreenLabel* label = new P::ScreenLabel(lineBegin, textSize, "mono");
+    
+    P::ScreenLabel* label = new P::ScreenLabel("", textSize, "mono");
+    
+    label->setColor( defaultColor.x,
+                     defaultColor.y,
+                     defaultColor.z, 1 );
+        
+    label->setPosition(4, static_cast<double>(lines_count*textSize)*(lineSpacing+1) );
+    screen->addChild(label);
+    
+    lines.push_back(label);
+    ++lines_count;
+    if ( visible_lines >= maxVisibleLines ) {
+        scroll(-1);        
+    }
+    else {
+        ++visible_lines;
+    }
+    if ( lines_count > maxLines ) {
+        this->clean(1);
+    }
+    
+}
+
+void ScreenConsole::scroll(int n) {
+    double offset = static_cast<double>((lines_count-visible_lines+n)*textSize) * (lineSpacing+1);
+    screen->setScreenOffset(0, -offset);
+    visible_lines += n;
+}
