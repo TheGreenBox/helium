764bed490364aa71eb3fff9cce52e82a1b1a0e48
AKindyakov <akindyakov@gmail.com>
2013-08-18 23:24:03 +0400

    add empty file for helium screen console

---
+++ modules/heliumScreenObjects/include/heliumScreenConsole.h
@@ -0,0 +1,9 @@
+/* ========================================================
+ * Organization: The Green Box
+ * 
+ * Project name: Helium
+ * File name:  heliumScreenConsole.h 
+ * Description: ScreenConsole Class
+ * Author:  AKindyakov 
+ * ========================================================
+ */
---
+++ modules/heliumScreenObjects/src/heliumScreenConsole.cpp
@@ -0,0 +1,9 @@
+/* ========================================================
+ * Organization: The Green Box
+ * 
+ * Project name: Helium
+ * File name:  heliumScreenConsole.cpp 
+ * Description: ScreenConsole Class
+ * Author:  AKindyakov 
+ * ========================================================
+ */
