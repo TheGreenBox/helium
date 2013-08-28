014f502a5f2b0e71804f12bbbe091b6157068d35
AKindyakov <akindyakov@gmail.com>
2013-08-29 00:35:20 +0400

    add Helium Global Instance header #17

---
+++ modules/heliumGameCore/include/heliumGameGlobal.h
@@ -0,0 +1,19 @@
+/* ========================================================
+ * Organization: The Green Box
+ * 
+ * Project name: Helium
+ * File name:    heliumGameGlobal.h 
+ * Description:
+ * Author:       AKindyakov 
+ * ========================================================
+ */
+
+#ifndef  HELIUM_GAME_GLOBAL_INCLUDED 
+#define  HELIUM_GAME_GLOBAL_INCLUDED
+
+class HeliumGlobal {
+public:
+private:
+};
+#endif // HELIUM_GAME_GLOBAL_INCLUDED 
+
