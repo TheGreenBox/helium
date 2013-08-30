794ee99a65025ae4b1e4ee0b59d4524be6be2da9
AKindyakov <akindyakov@gmail.com>
2013-08-30 11:45:08 +0400

    modify main input

--- tests/learn_test/pointer_test/main.cpp
+++ tests/learn_test/pointer_test/main.cpp
@@ -6,9 +6,13 @@ using namespace std;
 int playWithString(char** strPointer) {
     return 0;
 }
-
-int main() {
-    playWithString(&myString);
+/**
+* @param int argsNum - number of cmd input arguments
+* @param char** argAr - array of input string
+*/
+int main (int argsNum, char** argAr)
+    
+    
     std::cout << "hello world";
     return 0;
 }
