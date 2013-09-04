8fe85fb711fd513797139533cd7d1d7da32ced4b
AKindyakov <akindyakov@gmail.com>
2013-09-04 14:26:57 +0400

    add empty file for game objects world

---
+++ modules/heliumGameCore/include/heliumObjectsWorldIntefrace.h
@@ -0,0 +1,12 @@
+/* ========================================================
+ * Organization: The Green Box
+ * 
+ * Project name: Helium
+ * File name:    heliumObjectsWorldIntefrace.h 
+ * Description:
+ * Author:       AKindyakov 
+ * ========================================================
+ */
+#ifndef  heliumObjectsWorldIntefrace.h 
+#define  heliumObjectsWorldIntefrace.h 
+#endif // heliumObjectsWorldIntefrace.h 
