15bf5b33cee28a85a1a987edaade9e4e7a8eb79e
AKindyakov <akindyakov@gmail.com>
2013-09-08 14:31:17 +0400

    add develop test for scene world -> fork from first test test

---
+++ tests/sceneWorldDevelop/src/proGameObject.cpp
@@ -0,0 +1,67 @@
+/* ========================================================
+ * Organization: The Green Box
+ * 
+ * Project name: Helium
+ * File name:  wrapClasses.cpp 
+ * Description:
+ * Author:  AKindyakov 
+ * ========================================================
+ */
+#include <cmath>
+#include <iostream>
+#include "proGameObject.h"
+
+#include "pathGlobal.h"
+#include "heliumScreenObjects.h"
+#include "heliumGameGlobal.h"
+#include "heliumPreparedScreenButton.h"
+
+namespace P = Polycode;
+
+void AddDice::process() {
+}
+
+void AddBarrel::process() {
+}
+
+void AddBoll::process() {
+}
+
+void XCameraMove::process() {
+}
+
+void XNegativeCameraMove::process() {
+}
+
+void YCameraMove::process() {
+}
+
+void YNegativeCameraMove::process() {
+}
+
+using namespace P; // fucking macros !
+ProGameObject::ProGameObject( P::Core* _core )
+    : HeliumGameCore( _core ) 
+{
+    CoreServices::getInstance()->getResourceManager()->addArchive(g_helium_resource_path+"/default.pak");
+    CoreServices::getInstance()->getResourceManager()->addDirResource("default", false);
+    
+    keyboardInput->addEventHandler( P::KEY_1, new AddDice());
+    keyboardInput->addEventHandler( P::KEY_2, new AddBoll());
+    keyboardInput->addEventHandler( P::KEY_3, new AddBarrel());
+
+    keyboardInput->addEventHandler( P::KEY_d, new YCameraMove());
+    keyboardInput->addEventHandler( P::KEY_a, new YNegativeCameraMove());
+    keyboardInput->addEventHandler( P::KEY_w, new XCameraMove());
+    keyboardInput->addEventHandler( P::KEY_s, new XNegativeCameraMove());
+    
+    //objectWorld.getEngineScenePt()->getDefaultCamera()->setPosition(17,17,17);
+	//objectWorld.getEngineScenePt()->getDefaultCamera()->lookAt(Vector3(0,0,0));
+
+    HeliumExitScreenButton exitBt(this);
+    this->getScreenWorldPt()->addObject( &exitBt );
+}
+
+ProGameObject::~ProGameObject(){
+}
+
