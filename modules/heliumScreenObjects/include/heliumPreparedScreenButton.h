be2880062127e7a524cc78753a62c0019022f08b
SergNikitin <snikitin@outlook.com>
2013-09-21 17:30:16 +0400

    basic text button class added #21

--- modules/heliumScreenObjects/include/heliumPreparedScreenButton.h
+++ modules/heliumScreenObjects/include/heliumPreparedScreenButton.h
@@ -1,17 +1,20 @@
 /* ========================================================
  * Organization: The Green Box
- * 
+ *
  * Project name: Helium
- * File name:    heliumPreparedScreenButton.h 
+ * File name:    heliumPreparedScreenButton.h
  * Description:
- * Author:       AKindyakov 
+ * Author:       AKindyakov
  * ========================================================
  */
 #ifndef  HELIUM_PREPARED_SCREEN_BUTTON_INCLUDED
 #define  HELIUM_PREPARED_SCREEN_BUTTON_INCLUDED
 
+#include <Polycode.h>
 #include "heliumScreenButton.h"
 #include "heliumGameCore.h"
+
+namespace P = Polycode;
 
 struct HeliumPauseScreenButton : public PackagedScreenObject {
     HeliumPauseScreenButton ( HeliumGameCore* );
@@ -22,6 +25,12 @@ struct HeliumExitScreenButton : public PackagedScreenObject {
     HeliumExitScreenButton ( HeliumGameCore* );
     virtual ~HeliumExitScreenButton();
 };
+
+struct HeliumTextScreenButton : public PackagedScreenObject {
+    HeliumTextScreenButton ( HeliumGameCore*, P::String textLabel, int t_size, P::String font, int b_x_size, int b_y_size, int x_pos, int y_pos);
+    virtual ~HeliumTextScreenButton();
+};
+
 
 #endif // HELIUM_PREPARED_SCREEN_BUTTON_INCLUDED
 
--- modules/heliumScreenObjects/src/heliumPreparedScreenButton.cpp
+++ modules/heliumScreenObjects/src/heliumPreparedScreenButton.cpp
@@ -1,10 +1,10 @@
 /* ========================================================
  * Organization: The Green Box
- * 
+ *
  * Project name: Helium
- * File name:    heliumPreparedScreenButton.cpp 
+ * File name:    heliumPreparedScreenButton.cpp
  * Description:
- * Author:       AKindyakov 
+ * Author:       AKindyakov
  * ========================================================
  */
 
@@ -19,14 +19,14 @@ namespace P = Polycode;
 
 HeliumPauseScreenButton::HeliumPauseScreenButton (HeliumGameCore* gm) {
     const int b_size = 50;
-    P::ScreenShape* shape = new P::ScreenShape( P::ScreenShape::SHAPE_RECT, 
+    P::ScreenShape* shape = new P::ScreenShape( P::ScreenShape::SHAPE_RECT,
                                                  b_size, b_size);
     shape->setPosition( gm->getEngineCorePt()->getXRes()/2,
                          gm->getEngineCorePt()->getYRes()/2);
-  
+
     shape->loadTexture( g_helium_resource_path + "/pause_button.png" );
     this->heliumAlife = new ScreenButton( shape, new PauseGame(), NULL );
-    
+
     this->entityType = PackagedScreenObject::ENTITY_COLLISION_ONLY;
     this->entityShapeType = P::PhysicsScreenEntity::ENTITY_RECT;
     this->alife = 1;
@@ -36,14 +36,14 @@ HeliumPauseScreenButton::~HeliumPauseScreenButton () {}
 
 HeliumExitScreenButton::HeliumExitScreenButton(HeliumGameCore* gm) {
     const int b_size = 50;
-    P::ScreenShape* shape = new P::ScreenShape( P::ScreenShape::SHAPE_RECT, 
+    P::ScreenShape* shape = new P::ScreenShape( P::ScreenShape::SHAPE_RECT,
                                                 b_size, b_size);
     shape->setPosition( gm->getEngineCorePt()->getXRes()-b_size/2,
                         gm->getEngineCorePt()->getYRes()-b_size/2 );
-    
+
     shape->loadTexture( g_helium_resource_path + "/exit_button.png" );
-    heliumAlife = new ScreenButton( shape, new EscapeGame(), NULL );
-    
+    this->heliumAlife = new ScreenButton( shape, new EscapeGame(), NULL );
+
     this->entityType = PackagedScreenObject::ENTITY_COLLISION_ONLY;
     this->entityShapeType = P::PhysicsScreenEntity::ENTITY_RECT;
     this->alife = 1;
@@ -51,3 +51,31 @@ HeliumExitScreenButton::HeliumExitScreenButton(HeliumGameCore* gm) {
 
 HeliumExitScreenButton::~HeliumExitScreenButton() {}
 
+HeliumTextScreenButton::HeliumTextScreenButton(HeliumGameCore* gm, P::String textLabel, int t_size, P::String font, int b_x_size, int b_y_size, int x_pos, int y_pos) {
+    // const int b_size = 50;
+    int xRes = gm->getEngineCorePt()->getXRes();
+    int yRes = gm->getEngineCorePt()->getYRes();
+
+    if (x_pos > xRes - b_x_size / 2) {
+        x_pos = xRes / 2;
+    }
+    if (y_pos > yRes - b_y_size / 2) {
+        y_pos = yRes / 2;
+    }
+
+    P::ScreenShape* shape = new P::ScreenShape( P::ScreenShape::SHAPE_RECT,
+                                                b_x_size, b_y_size);
+    P::ScreenLabel* buttonText = new ScreenLabel(textLabel, t_size, font, 0, false);
+
+    shape->addChild(buttonText);
+    shape->setPosition(x_pos, y_pos);
+    shape->loadTexture( g_helium_resource_path + "/dark_wood.jpg" );
+
+    heliumAlife = new ScreenButton( shape, new EscapeGame(), NULL );
+
+    entityType = PackagedScreenObject::ENTITY_COLLISION_ONLY;
+    entityShapeType = P::PhysicsScreenEntity::ENTITY_RECT;
+    alife = 1;
+}
+
+HeliumTextScreenButton::~HeliumTextScreenButton() {}
